843b1b3eca4fe8e903f7f98a0d161e765bffd27f
July Tao <tjjsilwolf@gmail>
2024-04-22 14:48:48 +0800

    Initial Commit

---
+++ CopyOfWordle/CopyOfWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyOfWordleApp.swift
+//  CopyOfWordle
+//
+//  Created by july on 2024/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CopyOfWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
